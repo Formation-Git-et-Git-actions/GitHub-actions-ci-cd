198c832a5fe187c83e5818b157a2ad8941e0de95
marolahy3103 <marolahy3103@gmail.com>
2024-09-03 11:07:49 +0100

    Add build and test Nodejs workflow

---
+++ test.js
@@ -0,0 +1,9 @@
+const greet = require('./index')
+console.log("Running tests ...");
+if(greet() === "Hello world"){
+    console.log("Test passed");
+    process.exit(0);
+}else{
+    console.log("Test failed")
+    process.exit(1);
+}
