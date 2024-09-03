272028d873ac91bde6ab16538b545690416f999e
marolahy3103 <marolahy3103@gmail.com>
2024-09-03 17:27:59 +0100

    Execute script python

---
+++ is_prime.py
@@ -0,0 +1,17 @@
+import sys
+
+def is_prime(n):
+    """Return True if the number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    number = int(sys.argv[1])
+    if is_prime(number):
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
