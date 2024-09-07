bfde906388b3043a32cbd758c499d866bca70f84
marolahy3103 <marolahy3103@gmail.com>
2024-09-07 12:33:15 +0100

    Verifications of three condiions

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ console.log("Running tests ...");
 if (greet() === "Hello world") {
     console.log("Test passed");
     process.exit(0);
+    
 } else {
     console.log("Test failed");
     process.exit(1);
